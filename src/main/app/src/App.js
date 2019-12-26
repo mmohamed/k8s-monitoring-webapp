84e9c8eca62e591e07d60d45b887a744ca702183
Marouan MOHAMED <medmarouen@gmail.com>
2019-12-27 00:07:09 +0100

    First Sign IN

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import SignIn from './components/SignIn';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Word !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+    <Router>
+        {/* <Link to={'/about'} className="nav-link">About</Link> */}
+          <Switch>
+              <Route exact path='/' component={Dashboard} />
+              <Route path='/signin' component={SignIn} />
+          </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/main/app/src/components/SignIn.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Copyright from './../common/Copyright'
+import AuthService from '../api/AuthService'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  
+  const classes = useStyles();
+
+  const [state , setState] = React.useState({
+    username: '',
+    password: '',
+    rememberme: false
+  });
+
+  const handleChange = event => {
+    const newState = Object.assign({}, state);
+    newState[event.target.name] = event.target.value;
+    setState(newState);
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    
+    const credentials = {username: state.username, password: state.password};
+
+    AuthService.login(credentials).then(res => {
+        if(res.data.status === 200){
+            localStorage.setItem('userInfo', JSON.stringify(res.data.result));
+            this.props.history.push('/');
+        }else {
+            setState({message: res.data.message});
+        }
+    });
+  }
+
+  const validateForm = function() {
+    return state.username.length > 0 && state.password.length > 0;
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="username"
+            autoComplete="email"
+            autoFocus
+            value={state.username}
+            onChange={handleChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={state.password}
+            onChange={handleChange}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember"  color="primary" />}
+            label="Remember me"
+            name="rememberme" 
+            id="rememberme"
+            value={state.rememberme}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={!validateForm()}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
+}
