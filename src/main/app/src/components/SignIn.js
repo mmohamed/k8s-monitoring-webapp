c1873286eb88f1cf2f35ced201b8f41789211adb
Marouan MOHAMED <medmarouen@gmail.com>
2020-03-10 17:32:23 +0100

    Add pod/node/cluster service and controller

--- src/main/app/src/components/SignIn.js
+++ src/main/app/src/components/SignIn.js
@@ -42,8 +42,8 @@ export default function SignIn(props) {
   const classes = useStyles();
 
   const [state , setState] = React.useState({
-    username: 'medinvention',
-    password: 'yesyes',
+    username: '',
+    password: '',
     rememberme: false,
   });
 
